16366f332ed104f4c7230491bedc532d685b9763
Riister <56375685+Riister@users.noreply.github.com>
2019-10-09 19:40:59 -0700

    Add files via upload

---
+++ AddingMachine.java
@@ -0,0 +1,59 @@
+package cse360assign2;
+
+public class AddingMachine {
+	
+	/**
+	* @author cian villanueva
+	* ASUID: 1212770570
+	*/
+	
+	private int total;
+	
+	/**
+	* Defualt constructor for AddingMachine class.
+	*/
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	* Returns total for AddingMachine class.
+	*/
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	* Adds a value to the total.
+	*/
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	* Subtracts a value from the total.
+	*/
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	* Returns the total as a string.
+	*/
+		
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	* Sets total to 0.
+	*/
+	
+	public void clear() {
+	
+	}
+}
